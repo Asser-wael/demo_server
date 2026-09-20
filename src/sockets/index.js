dde5dbc708d5e4436043d4e68103d168431ab496
Asser-wael <aserweal@gmail.com>
2026-09-20 04:59:48 +0300

    update client

--- src/sockets/index.js
+++ src/sockets/index.js
@@ -6,7 +6,7 @@ let io;
 
 const allowedOrigins = [
   process.env.CLIENT_URL,
-  "https://demo-client-ashen.vercel.app",
+  "https://demo.cmcsweb.online",
 ].filter(Boolean);
 
 const initSocket = (server) => {
